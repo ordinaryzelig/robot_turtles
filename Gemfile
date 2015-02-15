e891c06a3d9ef88de04e46c84ca431a96c8a7954
Jared Ning <jared@redningja.com>
2015-02-15 16:20:13 -0600

    Add lotus app. Render empty board.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,13 @@ source 'https://rubygems.org'
 
 gem 'rake'
 
+gem 'lotus-router'
+gem 'lotus-controller'
+gem 'lotus-view'
+
+gem 'haml'
+gem 'sass'
+
 group :test do
   gem 'minitest', require: 'minitest/autorun'
   gem 'awesome_print'
--- app.rb
+++ app.rb
@@ -1,2 +1,65 @@
 require_relative 'load_path'
 require 'models'
+
+require 'haml'
+require 'sass'
+
+require 'lotus-controller'
+require 'lotus-view'
+require 'lotus-router'
+
+Lotus::View.configure do
+  root 'view_templates'
+end
+Lotus::View.load!
+
+module Games
+
+  module Actions
+    class Show
+
+      include Lotus::Action
+
+      expose :board
+      expose :turtle
+
+      def call(params)
+        @board = Board.new
+        @turtle = Turtle.new(:blue, :east)
+        @board.place @turtle, 0, 0
+        self.body = Games::Show.render(format: :html, **exposures)
+      end
+
+    end
+  end
+
+  class Show
+    include Lotus::View
+    layout :application
+  end
+
+end
+
+class ApplicationLayout
+  include Lotus::Layout
+end
+
+class Stylesheets
+
+  include Lotus::Action
+
+  def call(params)
+    self.body = Application.render(format: :css)
+  end
+
+  class Application
+    include Lotus::View
+  end
+
+end
+
+RobotTurtles =
+  Lotus::Router.new do
+    get '/', to: Games::Actions::Show
+    get '/application.css', to: Stylesheets
+  end
